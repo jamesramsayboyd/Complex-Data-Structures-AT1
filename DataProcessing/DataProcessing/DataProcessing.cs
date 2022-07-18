d34e3617ef37dccfd86d12b71e35608b48b7a725
James Boyd <jamesramsayboyd@gmail.com>
2022-07-18 10:10:47 +0800

    Created C# project

---
+++ DataProcessing/DataProcessing/DataProcessing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// James Boyd 30041547
+// Cluster - Complex Data Structures Assessment 1
+// A Windows Forms App using two LinkedList<Double> and demonstrating the
+// efficiency of several search and sort methods
+
+namespace DataProcessing
+{
+    public partial class DataProcessing : Form
+    {
+        public DataProcessing()
+        {
+            InitializeComponent();
+        }
+    }
+}
